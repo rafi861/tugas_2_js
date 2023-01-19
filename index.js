3161d7db19ff8adc83511a68b5ef09cd8aaf5bdb
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-01-19 21:09:30 +0700

    tugas 2 js

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var number = 50;
 var string = "huruf";
 var panjang = false;
 var km = 10;
+var ok = null;
+var yes;
 
 if(km>9)
 panjang = true;
 else {
   panjang = false;
 }
-var
+
 console.log(number);
 console.log(string);
 console.log(panjang);
+console.log(ok);
+console.log(yes);
